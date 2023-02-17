cfe3a8d8aedbabbd6b2ce12e48a80c7e4590bc6c
David Valero <davidvalero17@hotmail.com>
2023-02-17 19:36:50 +0100

    finalizacion tablero a falta de decorarlo

--- js/board.js
+++ js/board.js
@@ -8,42 +8,39 @@
  */
 
 //Recuperamos los datos del session storage.
-const nombresJugadores = JSON.parse(sessionStorage.getItem('jugadores'));
+const nombresJugadores = JSON.parse(sessionStorage.getItem("jugadores"));
 
 //Sobreescribimos el HTML.
 document.getElementById("player-one-name").innerHTML = nombresJugadores.player1;
 document.getElementById("player-two-name").innerHTML = nombresJugadores.player2;
 
-//Comienza lógica del tablero.
+
 
 //Vamos a empezar con el temporizador de turnos:
 
 let temporizador = false;
-let timer = 10;
+let timer = 60;
 let muestraTimer = document.getElementById("contador");
 let finTimer = null;
 
-
 const cuentaTiempo = () => {
+  finTimer = setInterval(() => {
+    timer--;
+    muestraTimer.innerHTML = timer;
+    if (timer == 0) {
+      clearInterval(finTimer);
+      mostrarResultado('Se os acabó el tiempo');
+      //AÑADIR UN FIN DEL JUEGO POR TIMEOUT
+    }
+  }, 1000);
+};
 
-    finTimer = setInterval(()=>{
-        timer--;
-        muestraTimer.innerHTML = timer;
-        if(timer == 0){
-            clearInterval(finTimer);
-//AÑADIR UN FIN DEL JUEGO POR TIMEOUT
-        }
-    }, 1000);
-
+if (temporizador == false) {
+  cuentaTiempo();
+  temporizador = true;
 }
 
-if(temporizador == false){
-    cuentaTiempo();
-    temporizador = true;  
-}
-
-
-//EJEMPLO DEL PROFESOR
+//Aqui comienza la lógica del tablero
 
 let tablero = Array.from(document.getElementsByClassName("celdas"));
 
@@ -52,66 +49,75 @@ let turno = true;
 let fichaP1 = 3;
 let fichaP2 = 3;
 
-let miTablero = ["","","","","","","","",""];
+let miTablero = ["", "", "", "", "", "", "", "", ""];
 
 let combinacionGanadora = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
 ];
 
-
 tablero.map((celda) => {
-        celda.addEventListener('click', ()=> {
+  celda.addEventListener("click", () => {
+    if (celda.innerHTML === "" && (fichaP1 > 0 || fichaP2 > 0)) {
+      celda.innerHTML = turno ? "X" : "O";
 
-            if((celda.innerHTML === "") && (fichaP1 > 0 || fichaP2 > 0)){
-                celda.innerHTML = (turno) ? "X" : "O";
+      turno ? fichaP1-- : fichaP2--;
 
-                (turno) ? fichaP1-- : fichaP2--;
+      miTablero[celda.id - 1] = turno ? "X" : "O";
 
-                miTablero[celda.id-1] = (turno) ? "X" : "O";
+      comprueboGanador();
 
-                comprueboGanador();
-
-                //Cambiamos de turno
-                turno = !turno;
-            }
-        })
+      //Change turno
+      turno = !turno;
     }
-)
+  });
+});
 
+//Comprueba ganador
 
-//Comprueba ganador by Adrian 
+const mostrarResultado = (resultado) => {
+    alert(resultado);
+};
 
 const comprueboGanador = () => {
+  //Comprueba X
+  const checkX = [];
+  miTablero.map((value, i) => {
+    if (value === "X") checkX.push(i);
+  });
+//Comprueba O
+  const checkO = [];
+  miTablero.map((value, i) => {
+    if (value === "O") checkO.push(i);
+  });
 
-    //X cells
-    const checkX = []
-    miTablero.map((value, i) => {
-        if(value === "X") checkX.push(i);
-    });
-    
-    const checkO = []
-    miTablero.map((value, i) => {
-        if(value === "O") checkO.push(i);
-    });
-    
-    //Iterating 'combinacionGanadora' to check player's board
-    combinacionGanadora.map((combinacion) => {
-        console.log(combinacion.toString(),checkO,checkX);
-            if(checkX.toString() === combinacion.toString()){
-            console.log("has ganado");
-        }
-            if(checkO.toString() === combinacion.toString()){
-            console.log("has ganado");
-        }   if(checkO.toString() !== combinacion.toString() && fichaP1 === 0 && fichaP2 === 0 && checkX.toString() !== combinacion.toString()){
-            console.log("Empatasteis");
-        }
-    });
+
+
+  let resultado = "Habeis quedado empate";
+  
+
+
+  //Aqui empieza la combinacion ganadora
+  combinacionGanadora.map((combinacion) => {
+    console.log(combinacion.toString(), checkO, checkX);
+    if (checkX.toString() === combinacion.toString()) {
+      resultado = nombresJugadores.player1;
+      mostrarResultado(`Ha ganado ${nombresJugadores.player1}`);
+    }
+    if (checkO.toString() === combinacion.toString()) {
+      resultado = nombresJugadores.player2;
+      mostrarResultado(`Ha ganado ${nombresJugadores.player2}`);
+    }
+  });
+  if (fichaP1 === 0 && fichaP2 === 0 && resultado === "Habeis quedado empate") {
+    mostrarResultado(resultado)
+  }
 };
-    
+
+
